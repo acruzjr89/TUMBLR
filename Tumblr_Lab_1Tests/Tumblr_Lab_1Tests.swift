50567bb6c3951a0f89aa6d9b556d499bfc7fa764
Your Name <dang_it_tati@yahoo.com>
2018-10-07 19:58:18 -0700

    Initial Commit

---
+++ Tumblr_Lab_1Tests/Tumblr_Lab_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Tumblr_Lab_1Tests.swift
+//  Tumblr_Lab_1Tests
+//
+//  Created by Tatiana Pantoja on 10/7/18.
+//  Copyright © 2018 Alejandro Cruz. All rights reserved.
+//
+
+import XCTest
+@testable import Tumblr_Lab_1
+
+class Tumblr_Lab_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
